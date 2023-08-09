03e689f4286fded6d35aafcb18d3d2deb5060459
rares <raresc589@gmail.com>
2023-08-09 23:47:45 +0300

    modified lexer_es.py so it wouldn't tokenize numbers, symbols etc. inside of strings

--- src/lexer_es.py
+++ src/lexer_es.py
@@ -10,72 +10,87 @@ class Token:
 # Lexer
 def lexer(input_text):
     tokens = []
+    str_enable=False
     current_pos = 0
+    keyword = ''
 
     while current_pos < len(input_text):
         char = input_text[current_pos]
 
-        if char.isdigit():
-            digits = ''
-            while current_pos < len(input_text) and input_text[current_pos].isdigit():
-                digits += input_text[current_pos]
-                current_pos += 1
-            tokens.append(Token(TokenType.INTEGER, int(digits)))
-        
-        elif char == '+':
-            tokens.append(Token(TokenType.PLUS, char))
-            current_pos += 1
-        
-        elif char == '-':
-            tokens.append(Token(TokenType.MINUS, char))
-            current_pos += 1
-        
-        elif char == '*':
-            tokens.append(Token(TokenType.MULTIPLICATION, char))
-            current_pos += 1
-        
-        elif char == '/':
-            tokens.append(Token(TokenType.DIVIDE, char))
-            current_pos += 1
-
-        elif char == '=':
-            tokens.append(Token(TokenType.EQUALS, char))
-            current_pos += 1
-
-        elif char.isspace():
-            current_pos += 1
-        
-        elif char.isalpha() or char=='"' or char=="'":
-            word = ''
-            if current_pos < len(input_text):
-                try:
-                    while input_text[current_pos].isalpha() or input_text[current_pos]=='"' or input_text[current_pos]=="'":
-                        word += input_text[current_pos]
-                        current_pos += 1
-                except:
-                    if word == 'print':
-                        tokens.append(Token(TokenType.PRINT, word))
-                    elif word == 'def':
-                        tokens.append(Token(TokenType.DEFINE, word))
-                    elif word in var_class.var:
-                        tokens.append(Token(TokenType.VARIABLE, word))
-                    elif word.startswith('"') and word.endswith('"') or word.startswith("'") and word.endswith("'"):
-                        tokens.append(Token(TokenType.STRING, word))
-                    else:
-                        tokens.append(Token(TokenType.UNEXPECTED_KEYWORD, word))
-                    tokens.append(Token(TokenType.EOF, None))
-                    return tokens
-            
-            if word == 'print':
-                tokens.append(Token(TokenType.PRINT, word))
-            elif word == 'def':
-                tokens.append(Token(TokenType.DEFINE, word))
-            elif word in var_class.var:
-                tokens.append(Token(TokenType.VARIABLE, word))
-            elif word.startswith('"') and word.endswith('"') or word.startswith("'") and word.endswith("'"):
-                tokens.append(Token(TokenType.STRING, word))
+        if char=='"' or char=="'":
+            if str_enable:
+                str_enable=False
+                keyword+=char
+                current_pos+=1
+                tokens.append(Token(TokenType.STRING,keyword))
             else:
-                tokens.append(Token(TokenType.UNEXPECTED_KEYWORD, word))
+                str_enable=True
+        
+        if not str_enable:
 
+            if char.isalpha() or char=='"' or char=="'":
+                word = ''
+                if current_pos < len(input_text):
+                    try:
+                        while input_text[current_pos].isalpha() or input_text[current_pos]=='"' or input_text[current_pos]=="'":
+                            word += input_text[current_pos]
+                            current_pos += 1
+                    except:
+                        if word == 'print':
+                            tokens.append(Token(TokenType.PRINT, word))
+                        elif word == 'def':
+                            tokens.append(Token(TokenType.DEFINE, word))
+                        elif word in var_class.var:
+                            tokens.append(Token(TokenType.VARIABLE, word))
+                        elif word.startswith('"') and word.endswith('"') or word.startswith("'") and word.endswith("'"):
+                            tokens.append(Token(TokenType.STRING, word))
+                        else:
+                            tokens.append(Token(TokenType.UNEXPECTED_KEYWORD, word))
+                        tokens.append(Token(TokenType.EOF, None))
+                        return tokens
+                
+                if word == 'print':
+                    tokens.append(Token(TokenType.PRINT, word))
+                elif word == 'def':
+                    tokens.append(Token(TokenType.DEFINE, word))
+                elif word in var_class.var:
+                    tokens.append(Token(TokenType.VARIABLE, word))
+                elif word.startswith('"') and word.endswith('"') or word.startswith("'") and word.endswith("'"):
+                    tokens.append(Token(TokenType.STRING, word))
+                else:
+                    tokens.append(Token(TokenType.UNEXPECTED_KEYWORD, word))
+
+            if char.isdigit():
+                digits = ''
+                while current_pos < len(input_text) and input_text[current_pos].isdigit():
+                    digits += input_text[current_pos]
+                    current_pos += 1
+                tokens.append(Token(TokenType.INTEGER, int(digits)))
+            
+            elif char == '+':
+                tokens.append(Token(TokenType.PLUS, char))
+                current_pos += 1
+            
+            elif char == '-':
+                tokens.append(Token(TokenType.MINUS, char))
+                current_pos += 1
+            
+            elif char == '*':
+                tokens.append(Token(TokenType.MULTIPLICATION, char))
+                current_pos += 1
+            
+            elif char == '/':
+                tokens.append(Token(TokenType.DIVIDE, char))
+                current_pos += 1
+
+            elif char == '=':
+                tokens.append(Token(TokenType.EQUALS, char))
+                current_pos += 1
+
+            elif char.isspace():
+                current_pos += 1
+        else:
+            keyword+=char
+            current_pos += 1
     tokens.append(Token(TokenType.EOF, None))
     return tokens
